5e8185a332d793fb1badc6c27de4d67f3e6db395
Наранхүү <codesaur@gmail.com>
2020-12-22 17:00:21 +0800

    private file path bug fixed

--- velociraptor/file/PrivateFileController.php
+++ velociraptor/file/PrivateFileController.php
@@ -7,11 +7,11 @@ class PrivateFileController extends FileController
     public function setFolder(string $folder, bool $relative = true)
     {
         $this->local = _document . '/../private' . $folder;
-        $this->public = single::app()->getWebUrl($relative) . '/private' . $folder;
+        $this->public = single::app()->getWebUrl($relative) . '/private/file?name=' . \urlencode($folder);
     }
- 
+    
     public function getPathUrl(string $fileName) : string
     {
-        return $this->public . '?name=' . \urlencode($fileName);
+        return $this->public . \urlencode("/$fileName");
     }
 }
